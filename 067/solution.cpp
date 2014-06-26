9a1dc01ded8c607794f9d4d7f17576f644f9a620
Joeseph Goodknight <jgoodknight@fas.harvard.edu>
2014-06-26 14:08:33 -0400

    triangle problems 18 and 67 finished (but unverified)

--- 067/solution.cpp
+++ 067/solution.cpp
@@ -1,6 +1,6 @@
 //Project Euler
 //
-//PROBLEM 18
+//PROBLEM 67
 //Solution by Joseph Goodknight
 //j@JosephGoodknight.com
 
@@ -13,28 +13,9 @@
 //
 // That is, 3 + 7 + 4 + 9 = 23.
 //
-// Find the maximum total from top to bottom of the triangle below:
+// Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
 //
-// 75
-// 95 64
-// 17 47 82
-// 18 35 87 10
-// 20 04 82 47 65
-// 19 01 23 75 03 34
-// 88 02 77 73 07 63 67
-// 99 65 04 28 06 16 70 92
-// 41 41 26 56 83 40 80 70 33
-// 41 48 72 33 47 32 37 16 94 29
-// 53 71 44 65 25 43 91 52 97 51 14
-// 70 11 33 28 77 73 17 78 39 68 17 57
-// 91 71 52 38 17 14 91 43 58 50 27 29 48
-// 63 66 04 68 89 53 67 30 73 16 69 87 40 31
-// 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-//
-// NOTE: As there are only 16384 routes, it is possible to solve this problem
-//by trying every route. However, Problem 67, is the same challenge with a
-//triangle containing one-hundred rows; it cannot be solved by brute force, and
-//requires a clever method! ;o)
+// NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
 
 #include <fstream>
 #include <iostream>
@@ -88,8 +69,8 @@ int main()
 		myTree.push_back(thisLevel);
 	}
 
-	printTree(myTree);
-	std::cout << "\n";
+	// printTree(myTree);
+	// std::cout << "\n";
 
 	//starting on the second level of the tree, we will look at the previous
 	//levels and add the maximum possible amount from the two possibilities
@@ -115,7 +96,7 @@ int main()
 
 	}
 
-	printTree(myTree);
+	// printTree(myTree);
 
 	treeLevelList lastLevel = myTree[myTree.size() - 1];
 	int maxPath = *std::max_element(lastLevel.begin(), lastLevel.end());
